a178ee2afc54c83c1351ffb00e48f27c8c8c125a
caitinggui <1029645297@qq.com>
2018-01-13 13:43:21 +0800

    change log level

--- apps/main/view.py
+++ apps/main/view.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
 from sanic import Blueprint
 from sanic.response import text
 from .. import app
 
+logger = logging.getLogger(__name__)
 main_bp = Blueprint("main")
 
 
 @main_bp.get("/")
 async def index(request):
-    info = """
+    main_url = app.url_for("main.index")
+    info = f"""
     "{main_url}"  show all the url for this app
     """
-    return text(info.format(main_url=app.url_for("main.index")))
+    return text(info)
