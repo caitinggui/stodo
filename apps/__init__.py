18bbfd53dc8d08ca9becb883837b8cea98897b44
caitinggui <1029645297@qq.com>
2018-01-21 22:06:39 +0800

    close mysql pool after server stop

--- apps/__init__.py
+++ apps/__init__.py
@@ -15,7 +15,7 @@ logging.config.dictConfig(log_config)
 logger = logging.getLogger(__name__)
 
 
-app = Sanic(__name__)
+app = Sanec(__name__)
 
 
 def createApp():
@@ -32,3 +32,10 @@ def createApp():
 @app.listener("before_server_start")
 async def beforeStart(app, loop):
     app.db = await createMysqlPool(loop, configs.get("mysql").get("mydb"))
+
+
+@app.listener('after_server_stop')
+async def after_server_stop(app, loop):
+    logger.info("Closing aiomysql pool")
+    app.db.close()
+    await app.db.wait_closed()
