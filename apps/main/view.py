85f27c8ad29e70d632ad2256a907c3b5bd40535b
caitinggui <1029645297@qq.com>
2018-01-23 16:31:50 +0800

    更新首页

--- apps/main/view.py
+++ apps/main/view.py
@@ -17,10 +17,12 @@ async def index(request):
     users = app.url_for("user.UserListView")
     token = app.url_for("user.TokenView")
     todos = app.url_for("todo.TodoListView")
+    todo = app.url_for("todo.TodoView", todo_id=42)
     info = {
         f'{main_url}': "show all the url for this app",
         f"{users}": "POST: 注册; GET: 查看用户组信息; PUT: 更新用户信息; DELETE: 注销用户, 非登出，而是删除",
         f"{token}": "POST: 登陆; GET: 查看当前登陆信息; PUT: 更新登陆信息, DELETE: 登出",
-        f"{todos}": "所有计划的事情"
+        f"{todo}": "GET: 获取某条todo; DELETE: 删除该条todo; PUT: 更新该条todo",
+        f"{todos}": "POST: 创建todo; GET: 当前用户的所有todo"
     }
     return webJson(data=info)
